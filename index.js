7ede0c994f213b35cbc5c3de1e9e9eba66603846
Christian Finefield <me@cfinefield.me>
2016-03-04 02:26:39 -0600

    Made a lot of progress. Added Handlebars and Bulma.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+/* node modules */
 var express = require('express');
 var app = express();
+var bodyParser= require('body-parser');
+var expresshbs = require('express-handlebars');
+
+app.engine ('handlebars', expresshbs({defaultLayout:'main'}));
+app.set ('view engine','handlebars');
+
+
+app.use(bodyParser.urlencoded());
+
+var cards = [];
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home', {cards: cards});
+});
+
+app.get('/add', function (req, res) {
+  res.render('add');
+});
+
+app.get('/cards', function (req, res) {
+  res.send(cards);
+});
+
+app.post('/add', function (req, res) {
+  cards.push(req.body);
 });
 
 app.listen(1337, function() {
